7fe1bce51b39f19641e16f1ef50c197286bb0533
trannguyenquocsang-2405 <trannguyenquocsang@gmail.com>
2025-11-18 12:59:28 +0700

    Done & Test BE success

--- src/db.js
+++ src/db.js
@@ -1,8 +1,13 @@
 const { Pool } = require("pg");
 require("dotenv").config();
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // BẮT BUỘC với Render
+  },
 });
 
 module.exports = pool;
